a61e705f3e4f41c355cf890470ca9885b1878183
Asa Gilmore <asaagilmore@gmail.com>
2024-08-23 13:58:09 -0700

    fix build loss

--- nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerCLDLoss.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerCLDLoss.py
@@ -1,31 +1,43 @@
 from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 from nnunetv2.training.loss.clDice import clDice
 from nnunetv2.training.loss.compound_losses import DC_and_CE_loss
+from nnunetv2.training.loss.dice import MemoryEfficientSoftDiceLoss
 import torch
 
-class combinedLossCLD(torch.nn.Module):
-    def __init__(self, dc_ce_weight=1, cl_weight=1):
-        super(combinedLossCLD, self).__init__()
-        self.dc_ce = DC_and_CE_loss({'batch_dice': False, 'smooth': 1e-5, 'do_bg': False})
-        self.cl = clDice()
+
+class combinedLoss(torch.nn.Module):
+    def __init__(self, loss1: torch.nn.Module, loss2: torch.nn.Module, weight1=1, weight2=1):
+        super(combinedLoss, self).__init__()
+        self.loss1 = loss1
+        self.loss2 = loss2
 
         # norm so weights sum to 1
-        self.dc_ce_weight = dc_ce_weight / (dc_ce_weight + cl_weight)
-        self.cl_weight = cl_weight / (dc_ce_weight + cl_weight)
+        self.weight1 = weight1 / (weight1 + weight2)
+        self.weight2 = weight2 / (weight1 + weight2)
 
     def forward(self, net_output: torch.Tensor, target: torch.Tensor):
-        dc_ce_loss = self.dc_ce(net_output, target)
-        cl_loss = self.cl(net_output, target)
+        loss1 = self.loss1(net_output, target)
+        loss2 = self.loss2(net_output, target)
 
-        return self.dc_ce_weight * dc_ce_loss + self.cl_weight * cl_loss
+        return self.weight1 * loss1 + self.weight2 * loss2
 
 
 class nnUNetTrainerCLDLoss(nnUNetTrainer):
-    def _build_loss_clDice(self):
+
+    def _build_loss(self):
         if self.label_manager.has_regions:
             raise NotImplementedError("clDice loss is not implemented for sparse segmentation")
         else:
-            self.loss = combinedLossCLD(dc_ce_weight=1, cl_weight=1)
+            dc_ce = DC_and_CE_loss({'batch_dice': self.configuration_manager.batch_dice,
+                                   'smooth': 1e-5, 'do_bg': False, 'ddp': self.is_ddp}, {}, weight_ce=1, weight_dice=1,
+                                  ignore_label=self.label_manager.ignore_label, dice_class=MemoryEfficientSoftDiceLoss)
+            cl = clDice()
+
+            loss = combinedLoss(dc_ce, cl, weight1=1, weight2=1)
+
+        # not gonna try to compile this rn might do later if i feel like it
+
+        return loss
 
     def __init__(
         self,
@@ -36,6 +48,5 @@ class nnUNetTrainerCLDLoss(nnUNetTrainer):
         unpack_dataset: bool = True,
         device: torch.device = torch.device("cuda"),
     ):
-        self._build_loss = self._build_loss_clDice
         super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
         self.enable_deep_supervision = False
